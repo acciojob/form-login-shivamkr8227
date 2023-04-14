7d6c55616e763b495b97760eb3815b58a77f8eb5
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-14 22:43:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	var fname=document.getElementbyName(fname);
+	var lname=document.getElementbyName(lname);
 
+	var firname=fname.value;
+	var lasname=lname.value;
+
+	alert(firname + lasname);
+}
 }
